25d6201513d09e5d83205a904524deec8ffabd7f
dolphin2410 <dolphin2410@outlook.kr>
2023-01-27 15:10:47 +0900

    :bookmark:v1.0.0-rc.2
    styling non-github action

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 
+use colored::Colorize;
+
 use crate::{database::{get_notation, get_repo}, maven::search_maven};
 
 pub fn handle_search_command(selector: &str) -> Result<(), Box<dyn Error>> {
@@ -7,6 +9,11 @@ pub fn handle_search_command(selector: &str) -> Result<(), Box<dyn Error>> {
     let repo = get_repo(&selector);
     let meta = search_maven(&target, &repo)?;
 
-    println!("{}:{}:{}", meta.group_id, meta.artifact_id, meta.versioning.latest);
+    let artifact = format!("{}:{}:{}", meta.group_id, meta.artifact_id, meta.versioning.latest);
+
+    println!("\n\n");
+    println!("{}: {}", format!("Repository").blue(), repo);
+    println!("{}: {}", format!("Artifact").blue(), artifact);
+    println!("\n\n");
     Ok(())
 }
